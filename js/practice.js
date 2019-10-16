119ae1cea610e9b650650b9e1c9e4fbfa235b67b
Puthy-Ouk <puthy.ouk168@gmail.com>
2019-10-16 09:21:40 +0700

    Jquery get data

---
+++ js/practice.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // $('.btn').click(function(){
+    //     $('#js,.pnc,strong').toggle();
+       
+    // });
+    $('li:nth-child(1)').css('background-color','red');
+    $('li:nth-child(2)').css('background-color','orange');
+    $('li:nth-child(3)').css('background-color','pink');
+})
